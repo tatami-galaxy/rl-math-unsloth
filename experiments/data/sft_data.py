6dd23099042bdfcb5cf675b1f743684026fff5cf
Ujan <ujandev@gmail.com>
2025-11-05 13:19:41 +0000

    sft, rl expts

--- experiments/data/sft_data.py
+++ experiments/data/sft_data.py
@@ -1,1 +1,3 @@
-####
+# zwhe99/DeepMath-103K -> 103k problems with reasoning traces
+# AI-MO/NuminaMath-CoT -> 859k problems with reasoing traces
+# AI-MO/NuminaMath-TIR -> 72k problems with tool integrated reasoning (python, sympy)
